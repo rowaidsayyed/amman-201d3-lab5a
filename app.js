ab4b8393728df486786f520c4d1737400f2bd32d
rowidsayyed <rowidsayyed@gmail.com>
2020-02-21 21:54:33 +0200

    q5(multiplyArray function) updated

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
- testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -96,11 +96,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    return [multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0] ,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0]}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
