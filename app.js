cce85d683fd1f693d6c486daa6b11cc765a4cfa3
rowidsayyed <rowidsayyed@gmail.com>
2020-02-21 21:52:40 +0200

    q4(sumArray function) updated

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ return [sum(sum(a, b)[0],c)[0],multiply(multiply(a, b)[0],c)[0],`${a} and ${b} a
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
- testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -75,12 +75,12 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    return [sum(sum(sumArr[0],sumArr[1])[0],sumArr[2])[0],`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum(sum(sumArr[0],sumArr[1])[0],sumArr[2])[0]} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
